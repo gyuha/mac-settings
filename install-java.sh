4ff822b797bf1697f7b4c6434abb543f724a9d2c
gyuha <nicegyuha@gmail.com>
2022-11-24 15:02:48 +0900

    update

--- install-java.sh
+++ install-java.sh
@@ -2,14 +2,20 @@
 # Install jenv
 # REF : https://carpfish.tistory.com/entry/jEnv-Mac%EC%97%90-jEnv%EB%A1%9C-%EC%97%AC%EB%9F%AC-%EB%B2%84%EC%A0%84%EC%9D%98-Java-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
 # 설치 이후에 등록을 별로도 해 줘야 한다.
+# 아래아 같이 버전별 실행이 필요 함.
+#
+#    jenv add  /Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home/
+#
 
 
 declare -a Apps=(
 "jenv"
 "java"
+"gradle"
 "AdoptOpenJDK/openjdk/adoptopenjdk8"
 "AdoptOpenJDK/openjdk/adoptopenjdk11"
 "AdoptOpenJDK/openjdk/adoptopenjdk15"
+"AdoptOpenJDK/openjdk/adoptopenjdk16"
 )
 
 
