0fb17a4f04748f646681b3bbe87f0255c5791c96
gyuha <nicegyuha@gmail.com>
2026-01-11 16:42:47 +0900

    add nerd font installer

---
+++ install-nerd-fonts.ts
@@ -0,0 +1,4 @@
+#!/bin/bash
+# REF:  https://github.com/officialrajdeepsingh/nerd-fonts-installer
+#
+bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)"
