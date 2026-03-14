74d442827dab9fc5e0bf3cc6168a07963113b348
gyuha <nicegyuha@gmail.com>
2026-03-14 14:26:31 +0900

    update

---
+++ applications/lightpanda.sh
@@ -0,0 +1,5 @@
+# Lightpanda Browser
+# https://github.com/lightpanda-io/browser
+
+curl -L -o lightpanda https://github.com/lightpanda-io/browser/releases/download/nightly/lightpanda-aarch64-macos && \
+chmod a+x ./lightpanda
