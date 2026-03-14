591a450bfd8455290851192d8e2f972390458781
gyuha <nicegyuha@gmail.com>
2026-03-14 14:25:03 +0900

    update

---
+++ applications/snapzy.sh
@@ -0,0 +1,5 @@
+# Snapzy
+# Native macOS screenshots, recording, annotation, and editing from the menu bar.
+
+brew tap duongductrong/snapzy https://github.com/duongductrong/Snapzy
+brew install --cask snapzy
