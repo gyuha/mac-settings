bcdb9fe0476f996e6f6822867113f7a3a7ddf6bd
gyuha <nicegyuha@gmail.com>
2026-04-29 20:58:19 +0900

    update

--- conf/zsh_profile.sh
+++ conf/zsh_profile.sh
@@ -65,8 +65,8 @@ alias zl='zellij -l welcome'
 ## appliation
 alias oc='opencode'
 alias ge='gemini'
-alias co='codex --yolo --enable multi_agent'
+alias co='codex --full-auto --enable multi_agent'
 
-ccs() { command ccs "$@" --dangerously-skip-permissions; }
+ccs() { command ccs "$@" --permission-mode auto; }
 
 export PATH="/opt/homebrew/bin:$HOME/.settings/bin:$PATH"
